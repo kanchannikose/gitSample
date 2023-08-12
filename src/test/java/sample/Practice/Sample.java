6ae4998f3572be72a2e66313ae6ec76cbe67d2df
HP <HP@Kanchan>
2023-08-12 20:52:08 +0530

    for main framework -2nd checking.

---
+++ src/test/java/sample/Practice/Sample.java
@@ -0,0 +1,13 @@
+package sample.Practice;
+
+import org.testng.annotations.Test;
+
+public class Sample {
+	@Test
+	public void sample()
+	{
+		System.out.println("Hello this is my second commit");
+		System.out.println("Hello from AT");
+	}
+
+}
