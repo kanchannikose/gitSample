be72d88df298019a11777884c35262108e5f6d2e
HP <HP@Kanchan>
2023-08-12 20:39:40 +0530

    2nd suitaution

---
+++ src/test/java/sample/Practice/Sample.java
@@ -0,0 +1,14 @@
+package sample.Practice;
+
+import org.testng.annotations.Test;
+
+public class Sample {
+	@Test
+	
+	public void sample()
+	{
+		System.out.println("Hello this is my secon commit");
+		System.out.println("Hello from AT");
+	}
+
+}
